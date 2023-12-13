f7225aa1bbd057da5290bd9186489288bf9713c8
24take <takechi12231223@gmail.com>
2023-12-13 21:55:25 +0900

    format: 子コンポーネントにコードを移した

---
+++ components/responses/responses-list.vue
@@ -0,0 +1,92 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+const products = ref();
+defineProps({
+  id: String,
+  code: String,
+  name: String,
+  description: String,
+  image: String,
+  price: Number,
+  category: String,
+  quantity: Number,
+  inventoryStatus: String,
+  rating: Number,
+});
+products.value = [
+  {
+    id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5,
+  },
+];
+</script>
+
+<template>
+  <div class="card">
+    <div>
+      <h3 class="header">下書き一覧</h3>
+      <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
+        <Column
+          field="code"
+          header="anke-to名"
+          sortable
+          style="width: 41%"
+        ></Column>
+        <Column
+          field="name"
+          header="回答期限"
+          sortable
+          style="width: 22%"
+        ></Column>
+        <Column
+          field="category"
+          header="最終更新日時"
+          sortable
+          style="width: 22%"
+        ></Column>
+        <Column field="quantity" header="回答" style="width: 15%"></Column>
+      </DataTable>
+    </div>
+    <div>
+      <h3 class="header">回答済み一覧</h3>
+      <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
+        <Column
+          field="code"
+          header="anke-to名"
+          sortable
+          style="width: 41%"
+        ></Column>
+        <Column
+          field="name"
+          header="回答期限"
+          sortable
+          style="width: 22%"
+        ></Column>
+        <Column
+          field="category"
+          header="最終更新日時"
+          sortable
+          style="width: 22%"
+        ></Column>
+        <Column field="quantity" header="回答" style="width: 15%"></Column>
+      </DataTable>
+    </div>
+  </div>
+</template>
+
+<style>
+.card {
+  width: 90%;
+}
+.header {
+  padding: 20px;
+}
+</style>
--- pages/responses.vue
+++ pages/responses.vue
@@ -1,89 +1,9 @@
-<script lang="ts" setup>
-const products = ref();
-products.value = [
-  {
-    id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5,
-  },
-];
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="card">
-    <div>
-      <h3 class="header">下書き一覧</h3>
-      <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
-        <Column
-          field="code"
-          header="anke-to名"
-          sortable
-          style="width: 41%"
-        ></Column>
-        <Column
-          field="name"
-          header="回答期限"
-          sortable
-          style="width: 22%"
-        ></Column>
-        <Column
-          field="category"
-          header="最終更新日時"
-          sortable
-          style="width: 22%"
-        ></Column>
-        <Column
-          field="quantity"
-          header="回答"
-          sortable
-          style="width: 15%"
-        ></Column>
-      </DataTable>
-    </div>
-    <div>
-      <h3 class="header">回答済み一覧</h3>
-      <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
-        <Column
-          field="code"
-          header="anke-to名"
-          sortable
-          style="width: 41%"
-        ></Column>
-        <Column
-          field="name"
-          header="回答期限"
-          sortable
-          style="width: 22%"
-        ></Column>
-        <Column
-          field="category"
-          header="最終更新日時"
-          sortable
-          style="width: 22%"
-        ></Column>
-        <Column
-          field="quantity"
-          header="回答"
-          sortable
-          style="width: 15%"
-        ></Column>
-      </DataTable>
-    </div>
+  <div>
+    <ResponsesList></ResponsesList>
   </div>
 </template>
 
-<style>
-.card {
-  width: 90%;
-}
-.header {
-  padding: 20px;
-}
-</style>
+<style></style>
