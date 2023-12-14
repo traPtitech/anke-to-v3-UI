3ced7fc2b7e8ee59e6f77fd52742f916f34b4db3
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-12-14 22:49:22 +0900

    feat: ESLintに`vue/html-self-closing`を入れる

--- components/ui/form/index.vue
+++ components/ui/form/index.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <FormCheckbox></FormCheckbox>
-  <FormCheckbox></FormCheckbox>
-  <FormNumberField></FormNumberField>
-  <FormRadioButton></FormRadioButton>
-  <FormScaleSlider></FormScaleSlider>
-  <FormSwitch></FormSwitch>
-  <FormTextArea></FormTextArea>
-  <FormTextField></FormTextField>
+  <FormCheckbox/>
+  <FormCheckbox/>
+  <FormNumberField/>
+  <FormRadioButton/>
+  <FormScaleSlider/>
+  <FormSwitch/>
+  <FormTextArea/>
+  <FormTextField/>
 </template>
 
 <style lang="scss" scoped></style>
