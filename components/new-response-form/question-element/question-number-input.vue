d5c79c187896de758615398f84a7b4366baabe97
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-12-14 20:17:09 +0900

    feat: とりあえずNewResponseFormが動くようにした (コードはかなり汚い)

---
+++ components/new-response-form/question-element/question-number-input.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import type { ResponseSettingsNumber } from '~/components/new-response-form/new-response-form-settings';
+import type {
+  ResponseFormQuestionInvalid,
+  ResponseFormQuestionSettingsBase,
+  ResponseFormQuestionSettingsNumber,
+} from '~/components/new-response-form/questionnaire-settings';
+
+type ModelValue = ResponseSettingsNumber &
+  ResponseFormQuestionSettingsNumber &
+  ResponseFormQuestionSettingsBase &
+  ResponseFormQuestionInvalid;
+
+const props = defineProps<{
+  modelValue: ModelValue;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: ModelValue): void;
+}>();
+
+const question = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+});
+</script>
+
+<template>
+  <InputNumber
+    v-model="question.number"
+    class="question-number-input"
+    :class="{ 'p-invalid': question.isInvalid }"
+    placeholder="回答を入力"
+    :input-props="{ required: question.required }"
+  />
+</template>
+
+<style lang="scss" scoped>
+.question-number-input {
+  width: 100%;
+}
+</style>
