96458b05d22d9cad3de70a74591360af79f64b36
24take <takechi12231223@gmail.com>
2023-12-17 16:57:18 +0900

    feat:アンケート欄の全体がリンクになるように

--- components/administrates/administrates-list.vue
+++ components/administrates/administrates-list.vue
@@ -17,36 +17,44 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
 <template>
   <div class="card">
     <div class="card-content">
-      <h2>自分が管理者になっているアンケート</h2>
-      <ul v-for="(product, index) in products" :key="index">
-        <div id="container">
-          <div id="questionnaire_name">
-            <h3>{{ product.title }}</h3>
-          </div>
-          <p id="deadline">回答期限：{{ product.response_due_date_time }}</p>
-          <div id="remaining">
-            <div v-if="remainingTime >= 0">
-              <p>{{ test }}</p>
+      <div class="header">
+        <h2>自分が管理者になっているアンケート</h2>
+      </div>
+      <div class="container">
+        <a href="#">
+          <ul v-for="(product, index) in products" :key="index">
+            <div id="questionnaire">
+              <div id="questionnaire_name">
+                <h3>{{ product.title }}</h3>
+              </div>
+              <p id="deadline">
+                回答期限：{{ product.response_due_date_time }}
+              </p>
+              <div id="remaining">
+                <div v-if="remainingTime >= 0">
+                  <p>{{ test }}</p>
+                </div>
+                <div v-else class="closed">
+                  <p>回答期間終了</p>
+                </div>
+              </div>
+              <p id="last_modified">最終更新日：{{ product.modified_at }}</p>
+              <p id="description">
+                {{ product.description }}
+              </p>
+              <div id="all_responded">
+                <div v-if="product.all_responded == true">
+                  <p>未回答者：なし</p>
+                </div>
+                <div v-else class="remaining_unresponded">
+                  <p>未回答者：あり</p>
+                </div>
+              </div>
+              <button id="show_answer">></button>
             </div>
-            <div v-else class="closed">
-              <p>回答期間終了</p>
-            </div>
-          </div>
-          <p id="last_modified">最終更新日：{{ product.modified_at }}</p>
-          <p id="description">
-            {{ product.description }}
-          </p>
-          <div id="all_responded">
-            <div v-if="product.all_responded == true">
-              <p>未回答者：なし</p>
-            </div>
-            <div v-else class="remaining_unresponded">
-              <p>未回答者：あり</p>
-            </div>
-          </div>
-          <button id="show_answer">></button>
-        </div>
-      </ul>
+          </ul>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -55,7 +63,23 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
 .card-content {
   margin-bottom: 20px;
 }
-#container {
+.header {
+  margin-bottom: 20px;
+}
+.container {
+  position: relative;
+}
+.container a {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  color: inherit;
+  text-decoration: none;
+}
+
+#questionnaire {
   display: grid;
   grid-template-rows: 40px 30px 30px 100px;
   grid-template-columns: 10px 1fr 1fr 70px;
@@ -64,7 +88,8 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
   align-items: center;
   justify-content: left;
   width: 90%;
-  margin-top: 20px;
+  padding-top: 10px;
+  margin-bottom: 10px;
   margin-left: 2%;
   border: 1px solid #000;
   border-radius: 5px;
@@ -95,6 +120,7 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
 #show_answer {
   grid-row: 1 / 5;
   grid-column: 4;
+  justify-content: center;
   font-size: 2rem;
 }
 #last_modified {
@@ -113,7 +139,7 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
 }
 
 @media screen and (max-width: 1000px) {
-  #container {
+  #questionnaire {
     grid-template-rows: 40px 60px 60px 150px;
   }
   #description {
@@ -121,9 +147,10 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
   }
 }
 @media screen and (max-width: 600px) {
-  #container {
+  #questionnaire {
     grid-template-rows: 40px 40px 40px 40px 40px 150px;
     grid-template-columns: 10px 1fr 50px;
+    margin-left: 5%;
   }
   #questionnaire_name {
     grid-row: 1;
@@ -156,7 +183,7 @@ const test = formatRelativeDate(new Date('2023-12-24T00:00:00'));
     grid-row: 5;
     grid-column: 2;
   }
-  
+
   #show_answer {
     grid-row: 1 / 7;
     grid-column: 3;
