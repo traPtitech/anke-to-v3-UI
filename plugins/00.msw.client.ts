13bb5cf1c367d782add26fdfe9060d65bd615616
rei <h.watanabe360@gmail.com>
2024-05-10 15:30:10 +0900

    _nuxtをmswのハンドリングから除外

--- plugins/00.msw.client.ts
+++ plugins/00.msw.client.ts
@@ -4,6 +4,13 @@ import { handlers } from '~/mocks/handlers';
 export default defineNuxtPlugin(async () => {
   if (process.env.NODE_ENV === 'development') {
     const worker = setupWorker(...handlers);
-    await worker.start();
+    await worker.start({
+      onUnhandledRequest(req, print) {
+        if (req.url.includes('/_nuxt')) {
+          return;
+        }
+        print.warning();
+      },
+    });
   }
 });
