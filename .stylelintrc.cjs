ae16832ec446683a987a6c304f05734bf75544a3
cp-20 <n.naoki.1587@gmail.com>
2026-02-11 17:39:05 +0900

    stylelint の unknown rule を消した

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -7,8 +7,6 @@ module.exports = {
     'length-zero-no-unit': true,
     'media-feature-name-no-vendor-prefix': true,
     'shorthand-property-no-redundant-values': true,
-    // prettierのインラインスタイルの末尾のコロンを削除するとコンフリクトするため、回避設定
-    'declaration-block-trailing-semicolon': null,
     // ::v-deepエラー回避
     'selector-pseudo-class-no-unknown': null,
   },
