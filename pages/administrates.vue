e6f2d1d8bff773b91ef907bbdbc7dfc8b964af9b
cp-20 <n.naoki.1587@gmail.com>
2026-03-28 14:25:30 +0900

    `/administrates` 実装した

--- pages/administrates.vue
+++ pages/administrates.vue
@@ -1,5 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import QuestionnaireList from '~/components/ui/questionnaire-list/questionnaire-list.vue';
+import { useGetQuestionnaires } from '~/composables/type-fetch/anke-to/client';
+
+const { data, error } = useGetQuestionnaires();
+
+const administratedQuestionnaires = computed(
+  () =>
+    data.value?.questionnaires.filter(
+      (questionnaire) => questionnaire.is_administrated_by_me,
+    ) ?? [],
+);
+</script>
 
 <template>
-  <div />
+  <div>
+    <div v-if="error" class="error-message">
+      {{ error.message }}
+    </div>
+    <div v-else-if="!data">
+      <p>Loading...</p>
+    </div>
+    <QuestionnaireList v-else :questionnaires="administratedQuestionnaires" />
+  </div>
 </template>
