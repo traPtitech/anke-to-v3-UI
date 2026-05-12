27dfdd6d3d9c995e5a498a12e07adf61b4c386e8
cp-20 <n.naoki.1587@gmail.com>
2026-05-12 17:31:53 +0900

    fix: maxLength を null で送信してしまうバグを修正

--- components/questionnaire-form/form-control-element/text-input.vue
+++ components/questionnaire-form/form-control-element/text-input.vue
@@ -4,12 +4,19 @@ import type { QuestionSettingsText } from '../type';
 const question = defineModel<QuestionSettingsText>({
   required: true,
 });
+
+const maxLength = computed({
+  get: () => question.value.max_length,
+  set: (value) => {
+    question.value.max_length = value ?? undefined;
+  },
+});
 </script>
 
 <template>
   <div class="form-control-element-text-input">
     <InputText placeholder="回答 (1行テキスト)" disabled />
-    <InputNumber v-model="question.max_length" placeholder="最大文字数 (任意)" />
+    <InputNumber v-model="maxLength" placeholder="最大文字数 (任意)" />
   </div>
 </template>
 
--- components/questionnaire-form/form-control-element/text-long-input.vue
+++ components/questionnaire-form/form-control-element/text-long-input.vue
@@ -4,12 +4,19 @@ import type { QuestionSettingsTextLong } from '../type';
 const question = defineModel<QuestionSettingsTextLong>({
   required: true,
 });
+
+const maxLength = computed({
+  get: () => question.value.max_length,
+  set: (value) => {
+    question.value.max_length = value ?? undefined;
+  },
+});
 </script>
 
 <template>
   <div class="form-control-element-text-input">
     <TextArea placeholder="回答 (長文テキスト)" disabled />
-    <InputNumber v-model="question.max_length" placeholder="最大文字数 (任意)" />
+    <InputNumber v-model="maxLength" placeholder="最大文字数 (任意)" />
   </div>
 </template>
 
